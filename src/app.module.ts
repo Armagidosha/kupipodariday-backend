4e6e9812d96af335582cb98584329ec2ab0a71cb
Armagidosha <armgidosha@list.ru>
2024-03-06 23:27:43 +0300

    add imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,28 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import config from "./config/configuration";
 import { typeOrmConfigFactory } from "./config/typeorm.config";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
 import UsersModule from "./users/users.module";
+import { OffersModule } from "./offers/offers.module";
+import WishesModule from "./wish/wishes.module";
+import WishlistsModule from "./wishlist/wishlists.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [config]
+      load: [config],
     }),
 
     TypeOrmModule.forRootAsync({
       useClass: typeOrmConfigFactory,
-      imports: [ConfigModule],
       inject: [ConfigService],
     }),
     UsersModule,
     AuthModule,
+    OffersModule,
+    WishesModule,
+    WishlistsModule,
   ],
   providers: [],
 })
